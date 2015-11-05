628b51b704ec6167ee39c8eb2c7f7b388dcc124f
JJ <jjzeilmaker@gmail.com>
2015-11-05 16:35:15 +0100

    *

---
+++ app/src/main/java/com/example/selfinote/MainActivity.java
@@ -0,0 +1,169 @@
+package com.example.selfinote;
+
+import android.app.AlertDialog;
+import android.app.LoaderManager;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity
+implements LoaderManager.LoaderCallbacks<Cursor>
+{
+    private static final int EDITOR_REQUEST_CODE = 1001;
+    private CursorAdapter cursorAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.selfinote.R.layout.activity_main);
+
+        cursorAdapter = new NotesCursorAdapter(this, null, 0);
+
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setAdapter(cursorAdapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                Uri uri = Uri.parse(NotesProvider.CONTENT_URI + "/" + id);
+                intent.putExtra(NotesProvider.CONTENT_ITEM_TYPE, uri);
+                startActivityForResult(intent, EDITOR_REQUEST_CODE);
+            }
+        });
+
+        getLoaderManager().initLoader(0, null, this);
+
+        // Add dumy text on startup
+        // Needs fix, adds every start up now...
+        insertSampleDataStartUp();
+    }
+
+    private void insertSampleDataStartUp() {
+        insertNote("Bacon ipsum");
+        insertNote("Zombie ipsum");
+        restartLoader();
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI,
+                values);
+        assert noteUri != null;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(com.example.selfinote.R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case com.example.selfinote.R.id.action_create_sample:
+                insertSampleData();
+                break;
+            case com.example.selfinote.R.id.action_delete_all:
+                deleteAllNotes();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteAllNotes() {
+       // Delete all notes in selfinote
+        DialogInterface.OnClickListener dialogClickListener =
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int button) {
+                        if (button == DialogInterface.BUTTON_POSITIVE) {
+                            // Show toast: Formating...
+                            Toast.makeText(MainActivity.this,
+                                    getString(com.example.selfinote.R.string.all_deleted),
+                                    Toast.LENGTH_SHORT).show();
+
+                           // Get all note and delete
+                            getContentResolver().delete(
+                                    NotesProvider.CONTENT_URI, null, null
+                            );
+                            restartLoader();
+
+                            // Show toast: Aaand its gone
+                            Toast.makeText(MainActivity.this,
+                                    getString(com.example.selfinote.R.string.note_deleted),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(com.example.selfinote.R.string.are_you_sure))
+                .setPositiveButton(getString(android.R.string.yes), dialogClickListener)
+                .setNegativeButton(getString(android.R.string.no), dialogClickListener)
+                .show();
+    }
+
+    private void insertSampleData() {
+        // Add dummy text
+        insertNote("0655156322");
+        insertNote("Boodschappen:\nKoek\nBrood\nBanaan\nEi");
+        insertNote("Cupcake ipsum dolor sit. Amet ice cream muffin biscuit I love cake. Biscuit lollipop jelly-o caramels topping. Ice cream brownie liquorice. Cupcake lollipop apple pie muffin cupcake icing. Sugar plum macaroon jelly beans chocolate cake dragée cotton candy chocolate cake. Liquorice cotton candy bonbon tart apple pie fruitcake jelly beans gingerbread I love. ");
+        insertNote("Bacon ipsum dolor amet pastrami chuck frankfurter pork");
+        insertNote("Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.");
+        restartLoader();
+    }
+
+    private void restartLoader() {
+        getLoaderManager().restartLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, NotesProvider.CONTENT_URI,
+                null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        cursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
+    }
+
+    public void openEditorForNewNote(View view) {
+        Intent intent = new Intent(this, EditorActivity.class);
+        startActivityForResult(intent, EDITOR_REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == EDITOR_REQUEST_CODE && resultCode == RESULT_OK) {
+            restartLoader();
+        }
+    }
+}
